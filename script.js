c382856af1a7b4e4c3aeef9c37bf835d8a083f0f
Joely <sjoely131@gmail.com>
2023-11-29 18:27:09 -0300

    ook

--- script.js
+++ script.js
@@ -75,3 +75,81 @@ function lerArquivoCSV(callback) {
   lerArquivoCSV(function(data) {
     criarGraficosAnimados(data, 25); // Especifique o número desejado de itens por gráfico
   });
+
+  function lerArquivoCSV2(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          callback(xhr.responseText);
+        } else {
+          console.error('Erro ao carregar o arquivo CSV');
+        }
+      }
+    };
+    xhr.open('GET', 'temporada_data.csv', true); // Caminho relativo ao HTML
+    xhr.send();
+  }
+  
+  function criarGraficosAnimados2(data, maxItensPorGrafico) {
+    var linhas = data.split('\n');
+    var cabecalho = linhas[0].split(',');
+    var dataset = [];
+  
+    for (var i = 1; i < linhas.length; i++) {
+      var campos = linhas[i].split(',');
+      var obj = {};
+  
+      for (var j = 0; j < cabecalho.length; j++) {
+        obj[cabecalho[j]] = campos[j];
+      }
+  
+      dataset.push(obj);
+    }
+  
+    var totalItens = dataset.length;
+    var numGraficos = Math.ceil(totalItens / maxItensPorGrafico);
+  
+    for (var k = 0; k < numGraficos; k++) {
+      var inicio = k * maxItensPorGrafico;
+      var fim = (k + 1) * maxItensPorGrafico;
+      var dadosGrafico = dataset.slice(inicio, fim);
+  
+      var canvas = document.createElement('canvas');
+      canvas.id = 'animatedChart' + k;
+      document.getElementById('chartsContainer2017').appendChild(canvas);
+  
+      var ctx = canvas.getContext('2d');
+      var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: dadosGrafico.map(item => item.product_category_name),
+          datasets: [{
+            label: 'Quantidade',
+            data: dadosGrafico.map(item => parseInt(item.quantidade)), // Converte para número inteiro
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          animation: {
+            duration: 2000,
+            easing: 'easeInOutQuart'
+          },
+          scales: {
+            x: {
+              beginAtZero: true
+            },
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
+  
+  lerArquivoCSV2(function(data) {
+    criarGraficosAnimados2(data, 25); // Especifique o número desejado de itens por gráfico
+  });
